acaa8e1f4d9c9b3ea767ab81a08a28fedafb0e71
rcescueta <rachel.escueta@gmail.com>
2025-08-09 09:54:18 +0200

    Output only yield

--- run-consumer-vergleichsgebiete-region.py
+++ run-consumer-vergleichsgebiete-region.py
@@ -82,9 +82,9 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
 
     output_grids = {
         "Yield": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Evapotranspiration": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Act_ET": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Pot_ET": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
+        # "Evapotranspiration": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "Act_ET": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        # "Pot_ET": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
 
